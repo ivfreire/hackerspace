decede0c0641f8ae5a142bdf63b74448c83bbc0c
Ícaro Freire <ivfreire@stardust.home>
2021-01-08 07:17:42 -0300

    código

---
+++ www/js/calendar.js
@@ -0,0 +1,4 @@
+// Calendar code
+
+var calendar = $('table#calendar');
+var today = new Date();
---
+++ www/js/index.js
@@ -0,0 +1,40 @@
+
+// Roda quando a página terminar de carregar
+window.onload = () => {
+	$('#particles-js').width(window.innerWidth);
+	$('#particles-js').height(window.innerHeight);
+
+	// Carrega frases de efeitos
+	$.get("assets/motd.json", (data) => {
+		$('header div.text h2').html(data['motd'][ Math.floor( Math.random() * data['motd'].length ) ]);
+	});
+
+	Hackerspace.init();
+
+	$.get('assets/events.json', (data) => {
+		data['events'].forEach(event => {
+			var date = new Date(event['start']);
+			$('div#news').append(`
+					<div class='container'>
+						<a href="events/?event=${event['id']}">
+						<div class='content'>
+							<div class='header'>
+								<img src='assets/events/${event['id']}/header.jpg'/>
+							</div>
+							<div class='text'>
+								<div class='datetime'><h3>${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}</h3></div>
+								<div class='title'><h2>${event['title']}</h2></div>
+							</div>
+						</div>
+						</a>
+					</div>
+			`);
+		});
+	});
+
+	$('body').show();
+
+	var titlePos = '30%';
+	if (window.innerWidth < 624) titlePos = '50%';
+	$('header div.text').animate({ bottom: titlePos, opacity: 1.0 }, 500);
+}
