282c11880232f2e96960c7aab3cfd54e40b19c4b
Ícaro Freire <icarovf@usp.br>
2021-01-23 19:30:04 -0300

    novas animações

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,10 @@
 var Hackerspace = {
 	onload: function() {
+		$('header div.text').fadeIn(300);
+
 		setTimeout(() => {
 			$('div.indicator i').fadeIn(1000);
+			$('nav.navigation').fadeIn(200);
 		}, 1000);
 	},
 	onscroll: function() {
