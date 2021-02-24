0751e576e22397e29267735ba5c5852f87d05fe0
Icaro Freire <icarovf@usp.br>
2021-02-24 08:16:57 -0300

    social networks

--- www/js/script.js
+++ www/js/script.js
@@ -4,8 +4,9 @@ var Hackerspace = {
 
 		setTimeout(() => {
 			$('div.indicator i').fadeIn(1000);
-			$('nav.navigation').fadeIn(200);
-		}, 1000);
+			$('nav.navigation').fadeIn(300);
+			$('div.social').fadeIn(300);
+		}, 1500);
 	},
 	onscroll: function() {
 		if (window.scrollY == 0) $('div.indicator i').fadeIn(1000);
