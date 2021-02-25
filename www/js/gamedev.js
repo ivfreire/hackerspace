166b2d73a55d1a6396b028064f000d0c06557950
Icaro Freire <icarovf@usp.br>
2021-02-25 20:15:14 -0300

    code updates

--- www/js/gamedev.js
+++ www/js/gamedev.js
@@ -6,13 +6,13 @@ window.onload = function() {
 		data['games'].forEach(game => {
 			$('div#recents').append(`
 				<a href="${game['link']}">
-					<div>
-						<div class='thumb'>
+					<div class='box'>
+						<div class='image'>
 							<img src="assets/games/${game['id']}/thumb.jpg"/>
 						</div>
 						<div class='text'>
 							<h2>${game['title']}</h2><br>
-							<span>${game['year']}</span>
+							<span>${game['year']}</span><br><br>
 							<p>${game['description']}</p>
 						</div>
 					</div>
