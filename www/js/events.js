18c6fc5881155272d2f63fb506a1e3ec53434b5b
Icaro Freire <icarovf@usp.br>
2021-02-26 12:13:41 -0300

    loads events from events.json file

--- www/js/events.js
+++ www/js/events.js
@@ -1,4 +1,42 @@
 window.onload = function() {
+
+	// Carrega eventos
+	$.get('assets/events.json', function(data) {
+		let first = true;
+		data['events'].forEach(event => {
+			if (first) {
+				$('div.highlights#recents').append(`
+					<div class='highlight'>
+						<div class='info'>
+							<div>
+								<h2>${event['title']}</h2><br>
+								<span>${event['date']}</span><br><br>
+								<p>${event['description']}</p>
+							</div>
+						</div>
+						<div class='image'>
+							<img src='assets/events/${event['id']}/thumb.jpg'/>
+						</div>
+					</div>
+				`);
+				first = false;
+			} else {
+				$('div.boxes#recents').append(`
+					<div class='box'>
+						<div class='image'>
+							<img src='assets/events/${event['id']}/thumb.jpg'/>
+						</div>
+						<div class='text'>
+							<h2>${event['title']}</h2><br>
+							<span>${event['date']}</span><br><br>
+							<p>${event['description']}</p>
+						</div>
+					</div>
+				`);
+			}
+		})
+	});
+
 	$('body').show();
 	Hackerspace.onload();
 }
