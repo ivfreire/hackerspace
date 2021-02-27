a2c5819871ddf1f8ffc49c45cb54f5427643300d
Icaro Freire <icarovf@usp.br>
2021-02-27 16:03:20 -0300

    games data.json update

--- www/js/game.js
+++ www/js/game.js
@@ -1,5 +1,5 @@
 
-const loadGameData = function (id) {
+const loadGameData = function (id, callback) {
 	$.get(`assets/games/${id}/data.json`, function(game) {
 		$(document).attr('title', `${game['title']} - Hackerspace IFUSP`);
 		$('header').css('background-image', `url(../assets/games/${id}/thumb.jpg)`);
@@ -13,54 +13,68 @@ const loadGameData = function (id) {
 			<a href='${game['link']}'><button>${game['title']}</button></a>
 		`);
 		
-		game['developers'].forEach(dev => {
-			$('div#devs ul').append(`
-				<li>
-					<span>${dev['name']}</span>
-					<p>${dev['role']}</p>
-				</li>
-			`); 
-		});
+		if ('developers' in game) {
+			game['developers'].forEach(dev => {
+				$('div#devs ul').append(`
+					<li>
+						<span>${dev['name']}</span>
+						<p>${dev['role']}</p>
+					</li>
+				`); 
+			});
+		} else
+			$('div#devs').hide();
 
-		if ('screenshots' in game['images'])
+		if ('screenshots' in game['images']) {
 			game['images']['screenshots'].forEach(image => {
 				$('div#screenshots').append(`
 					<a href='assets/games/${id}/images/${image}'><div><img src='assets/games/${id}/images/${image}'/></div></a>
 				`);
 			});
+		} else
+			$('div.screenshots').hide();
 
-		game['platforms'].forEach(platform => {
-			if (platform['platform'] == 'windows') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-windows"></i><br><br><span>Windows 7/8/10</span></div></a>`);
-			if (platform['platform'] == 'linux') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-linux"></i><br><br><span>Linux</span></div></a>`);
-			if (platform['platform'] == 'mac') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-apple"></i><br><br><span>Mac</span></div></a>`);
-			if (platform['platform'] == 'android') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-android"></i><br><br><span>Android</span></div></a>`);
-			if (platform['platform'] == 'ios') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-app-store-ios"></i><br><br><span>iOS</span></div></a>`);
-			if (platform['platform'] == 'browser') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fas fa-globe"></i><br><br><span>Browser</span></div></a>`);
-		});
+		if ('platforms' in game) {
+			game['platforms'].forEach(platform => {
+				if (platform['platform'] == 'windows') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-windows"></i><br><br><span>Windows 7/8/10</span></div></a>`);
+				if (platform['platform'] == 'linux') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-linux"></i><br><br><span>Linux</span></div></a>`);
+				if (platform['platform'] == 'mac') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-apple"></i><br><br><span>Mac</span></div></a>`);
+				if (platform['platform'] == 'android') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-android"></i><br><br><span>Android</span></div></a>`);
+				if (platform['platform'] == 'ios') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fab fa-app-store-ios"></i><br><br><span>iOS</span></div></a>`);
+				if (platform['platform'] == 'browser') $('div#platforms').append(`<a href='${platform['link']}'><div><i class="fas fa-globe"></i><br><br><span>Browser</span></div></a>`);
+			});
+		} else
+			$('div.platforms').hide();
 
-		game['built-with'].forEach(tool => {
-			$('div#built-with').append(`
-				<a href="#">
-					<div>
-						${tool['icon']}<br><br>
-						<span>${tool['tool']}</span>
-					</div>
-				</a>
-			`);
-		});
+		if ('built-with' in game) {
+			game['built-with'].forEach(tool => {
+				$('div#built-with').append(`
+					<a href="#">
+						<div>
+							${tool['icon']}<br><br>
+							<span>${tool['tool']}</span>
+						</div>
+					</a>
+				`);
+			});
+		} else
+			$('div.built-with').hide();
 
 		game['tags'].forEach(tag => {
 			$('div#tags').append(`<input type='submit' value='${tag}'/>`);
 		});
+	
+		callback();
 	});
 }
 
 
 window.onload = function() {
 	let params = getUrlParameters(window.location.search);
-	loadGameData(params['g']);
-	$('body').show();
-	Hackerspace.onload();
+	loadGameData(params['g'], () => {
+		$('body').show();
+		Hackerspace.onload();
+	});
 }
 
 window.onscroll = function() {
