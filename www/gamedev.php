bb3ed90ba9012fcc0229118bc01feb44fdd014e7
Ícaro Freire <icarovf@outlook.com>
2022-03-28 13:12:44 -0300

    added team meambers

--- www/gamedev.php
+++ www/gamedev.php
@@ -18,210 +18,111 @@
 				<div class="wrapper">
 					<span>HS Gamedev</span>
 					<h1>Crie e explore novos universos.</h1>
-					<p>O HSGD é um projeto de extensão universitária dedicado à pesquisa e ao desenvolvimento de jogos digitais e ligado ao Hackerspace IFUSP.</p>
+					<p>O <b>HSGD</b> é um projeto de extensão universitária dedicado ao desenvolvimento de jogos digitais e ligado ao <b>Hackerspace</b> IFUSP.</p>
+					<div class="buttons">
+						<button>Nossa biblioteca</button>
+						<button>Faça parte!</button>
+					</div>
 				</div>
 			</div>
 		</header>
 
 		<div class="page">
-			<div class="stack">
+			<div class="stack">	
 
 				<div class="element darker">
 					<div class="wrapper">
+						<div class="panel presents" id="intro">
+							<div class="item" id="gamedev">
+								<div class="icon"><i class="fas fa-gamepad"></i></div>
+								<div class="title"><h2>Gamedev</h2></div>
+								<div class="text">
+									<p>Somos um grupo de alunos de graduação e pós-graduação interessados no desenvolvimento de jogos digitais.</p>
+								</div>
+							</div>
+							<div class="item" id="contribution">
+								<div class="icon"><i class="fas fa-question-circle"></i></div>
+								<div class="title"><h2>Contribuições</h2></div>
+								<div class="text">
+									<p>Jogos são mídias interativas poderosas que podem ser usadas nas áreas de educação, divulgação e pesquisa.</p>
+								</div>
+							</div>
+							<div class="item" id="tools">
+								<div class="icon"><i class="fas fa-tools"></i></i></div>
+								<div class="title"><h2>Ferramentas</h2></div>
+								<div class="text">
+									<p>As ferramentas de Gamedev são bem diversas. Há muitas opções. No HSGV priorizamos o uso de tecnologias <i>open-source</i>.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="element darker" style="padding-top: 0;">
+					<div class="wrapper">
+						<div class="panel presents" id="recents">
+							<!-- <div class="title"><h2>Títulos recentes</h2></div> -->
+							<div class="content">
+								<?php
+									$games = json_decode(file_get_contents('assets/games/games.json'), true);
+									$games = $games['games'];
+									if (isset($games)) {
+										for ($i = count($games) - 1; $i > count($games) - 5; $i--) {
+											echo "
+												<a href='game.php?g=".$games[$i]['id']."'>
+													<div class='item' title='".$games[$i]['title']."'>
+														<div class='image'><img src='assets/games/".$games[$i]['id']."/thumb.jpg'></div>
+													</div>
+												</a>
+											";
+										}
+									}
+								?>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="element dark" id="team">
+					<div class="wrapper">
 						<div class="panel">
-							<div class="title"><h2></h2></div>
+							<div class="title"><h2>Equipe</h2><br><p>Alunos que fazem parte do grupo de GameDev</p></div>
+							<div class="content">
+								<div id="team-wall">
+									<?php
+										$devs = json_decode(file_get_contents('assets/team.json'), true);
+										$devs = $devs['team'];
+										if (isset($devs)) {
+											foreach ($devs as $dev) {
+												echo "
+													<a href='https://twitter.com/".$dev['twitter']."'>
+														<div class='dev' title='".$dev['name']."'>
+															<img src='assets/team/".$dev['id'].".png'/>
+															<span>".$dev['id']."</span>
+															<span>".$dev['name']."</span>
+														</div>
+													</a>
+												";
+											}
+										}
+									?>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 
 				<div class="element darker">
 					<div class="wrapper">
-						<div class="panel presents" id="intro">
-							<div class="item">
-								<div class="icon"><i class="fas fa-gamepad"></i></div>
-								<div class="title"><h2>Gamedev</h2></div>
-								<div class="text">
-									<p>O grupo de Gamedev recém formado do <b>Hackerspace</b> é composto por alunos interessados no desenvolvimento de jogos digitais.</p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="icon"><i class="fas fa-question-circle"></i></div>
-								<div class="title"><h2>Contribuições</h2></div>
-								<div class="text">
-									<p>Jogos digitais têm um impacto imenso sobre a didática no ensino e divulgação científica para o público geral.</p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="icon"><i class="fas fa-tools"></i></i></div>
-								<div class="title"><h2>Ferramentas</h2></div>
-								<div class="text">
-									<p>Nós priorizamos o uso de ferramentas e <i>softwares</i> open-source no processo de desenvolvimento. Porém programas proprietários também são usados sem problemas.</p>
-								</div>
+						<div class="panel">
+							<div class="content">
+
 							</div>
 						</div>
 					</div>
 				</div>
 
 				<!--
-				<div class="element dark">
-					<div class="wrapper">
-						<div class="panel presents" id="intro">
-							<div class="item">
-								<div class="icon"><i class="fas fa-gamepad"></i></div>
-								<div class="title"><h2>Gamedev</h2></div>
-								<div class="text">
-									<p>O grupo de Gamedev recém formado do <b>Hackerspace</b> é composto por alunos interessados no desenvolvimento de jogos digitais.</p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="icon"><i class="fas fa-question-circle"></i></div>
-								<div class="title"><h2>Contribuições</h2></div>
-								<div class="text">
-									<p>Jogos digitais têm um impacto imenso sobre a didática no ensino e divulgação científica para o público geral.</p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="icon"><i class="fas fa-tools"></i></i></div>
-								<div class="title"><h2>Ferramentas</h2></div>
-								<div class="text">
-									<p>Nós priorizamos o uso de ferramentas e <i>softwares</i> open-source no processo de desenvolvimento. Porém programas proprietários também são usados sem problemas.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="element darker">
-					<div class="wrapper">
-						<div class="panel">
-							<div class="title">
-								<h2>Ferramentas</h2>
-							</div>
-							<div class="content">
-								<div id="tools">
-									<a href="https://www.gimp.org/">
-										<div>
-											<div class="image"><img src="images/icons/gimp.png" alt="gimp logo"></div>
-											<div class="title"><h3>GIMP</h3></div>
-											<div class="text"><p>Gimp é uma ótima ferramenta para edição e manipulação de imagens.</p></div>
-										</div>
-									</a>
-									<a href="https://blender.org/">
-										<div>
-											<div class="image"><img src="images/icons/blender.png" alt="blender logo"></div>
-											<div class="title"><h3>Blender</h3></div>
-											<div class="text"><p>Blender é um software de modelagem e renderização 3D, perfeito pra jogos mais complexos.</p></div>
-										</div>
-									</a>
-									<a href="https://godotengine.org/">
-										<div>
-											<div class="image"><img src="images/icons/godot.png" alt="godot logo"></div>
-											<div class="title"><h3>Godot</h3></div>
-											<div class="text"><p>Godot é uma <i>Game Engine</i> open-source que suporta jogos 2D e 3D.</p></div>
-										</div>
-									</a>
-									<a href="https://code.visualstudio.com">
-										<div>
-											<div class="image"><img src="images/icons/vscode.png" alt="vscode logo"></div>
-											<div class="title"><h3>Code</h3></div>
-											<div class="text"><p>O VS Code é um editor de texto open-source desenvolvido pela Microsoft baseado na IDE Visual Studio.</p></div>
-										</div>
-									</a>
-								</div>
-								<div class="highlights" id="feat">
-									<a href="assets/res" taget="_blank">
-										<div class="highlight icon">
-											<div class="info">
-												<div>
-													<h2>Galeria de Recursos</h2><br>
-													<p>Recursos como imagens, texturas, modelos 3d e mais que foram utilizados nos títulos do <b>Hackerspace</b>.</p>
-												</div>
-											</div>
-											<div class="image">
-												<img class="background" src="images/misc/gamedev_assets.png" alt="icone de recursos">
-											</div>
-										</div>
-									</a>
-									<a href="#">
-										<div class="highlight icon">
-											<div class="info">
-												<div>
-													<h2>Coding</h2><br>
-													<p>No processo de desenvolvimento de jogos programação e <i>coding</i> são muito importantes para escrever as mecânicas dos jogos e a lógica.</p>
-												</div>
-											</div>
-											<div class="image">
-												<img class="background" src="images/misc/gamedev_coding.png" alt="icone de programação">
-											</div>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="element darker" style="padding-top: 0;">
-					<div class="wrapper">
-						<div class="panel" id="demo">
-							<div class="title">
-								<h2>Demo</h2>
-							</div>
-							<div class="content">
-								<div class="left">
-									<div class="holder">
-										<iframe class="demo" src="//v6p9d9t4.ssl.hwcdn.net/html/3503202/index.html" frameborder="0" width="100%"></iframe>
-									</div>
-								</div>
-								<div class="right">
-									<h3>Hackerspace 2021</h3><br>
-									<p>Bem vindxs ao Hackerspace 2021!<br><br>O Hackerspace IFUSP é um laboratório aberto à comunidade acadêmica da Universidade. Localizado no prédio principal do Instituto de Física da USP.<br><br>Em 2021, o espaço não pôde ser aberto devido às restrições sanitárias causadas pela pandemia de COVID-19, porém, o HS decidiu fazer uma cerimônia de recepção dos calouros de 2021 online.<br><br>O projeto para 2021 é um curso de Gamedev online ofericido por alunos da graduação, ao lado está um protótipo de uma jogo desenvolvido pela equipe do HS.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="element darker" style="padding-top: 0;">
-					<div class="wrapper">
-						<div class="panel">
-							<div class="title">
-								<h2>Em breve</h2>
-							</div>
-							<div class="content">
-								<div class="highlights" id="coming-soon">
-									<a href="#">
-										<div class="highlight">
-											<div class="info">
-												<div>
-													<h2>Curso de Gamedev</h2><br>
-													<p>Curso de desenvolvimento de jogos digitais do <b>Hackerspace</b>.</p><br>
-												</div>
-											</div>
-											<div class="image">
-												<img class="background" src="images/misc/gamedev_course.png" alt="curso de gamedev do hackerspace">
-											</div>
-										</div>
-									</a>
-									<a href="#">
-										<div class="highlight">
-											<div class="info">
-												<div>
-													<h2>Hackerspace Game Jam</h2><br>
-													<p>Competição de Gamedev do <b>Hackerspace</b>.</p><br>
-												</div>
-											</div>
-											<div class="image">
-												<img class="background" src="images/misc/gamejam_icon.png" alt="gamejam hackerspace">
-											</div>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
 				<div class="element darker" style="padding-top: 0;">
 					<div class="wrapper">
 						<div class="panel">
