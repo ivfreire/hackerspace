918efe14c93d21eb9cbc2c45756c7aead19e15fd
Ícaro Freire <icarovf@outlook.com>
2022-03-30 10:03:08 -0300

    parallax on gamedev page

--- www/js/gamedev.js
+++ www/js/gamedev.js
@@ -1,11 +1,17 @@
 
 window.onload = function() {
 	$('body').show();
+
 	Hackerspace.onload();
 }
 
 window.onscroll = function() {
 	Hackerspace.onscroll();
+
+	let parallax = 0.4;
+	$('header').css({
+		'backgroundPositionY': - window.scrollY * parallax
+	});
 }
 
 var openGame = function(id) { window.location = `game.html?g=${id}`; }
